f331d682c2714e2618ccd0d772cac1adcaff19b5
Rajkumar Katanor <rkatanor@gmail.com>
2017-10-05 13:00:14 +0530

    Update SMSAPI.java

--- src/com/amr/net/SMSAPI.java
+++ src/com/amr/net/SMSAPI.java
@@ -1,77 +1,48 @@
-package com.amr.net;
+package com.test.networking;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.URLEncoder;
 
-public class SMSAPI {
-	
-	public void sendsms(){
-	// Replace with your username
-	String user = "AMR Fibernet";
-
-	// Replace with your API KEY (We have sent API KEY on activation email, also available on panel)
-	String apikey = "lrb00IoktsKI0wqvflEG";
-
-	// Replace with the destination mobile Number to which you want to send sms
-	String mobile = "9866984871";
-
-	// Replace if you have your own Sender ID, else donot change
-	String senderid = "MYTEXT";
-
-	// Replace with your Message content
-	String message = "Test sms API";
-
-	// For Plain Text, use "txt" ; for Unicode symbols or regional Languages like hindi/tamil/kannada use "uni"
-	String type="txt";
-
-	//Prepare Url
-	URLConnection myURLConnection=null;
-	URL myURL=null;
-	BufferedReader reader=null;
-
-	//encoding message 
-	String encoded_message=URLEncoder.encode(message);
-
-	//Send SMS API
-	String mainUrl="http://smshorizon.co.in/api/sendsms.php?";
-
-	//Prepare parameter string 
-//http://smshorizon.co.in/api/sendsms.php?user=AMR Fibernet&apikey=lrb00IoktsKI0wqvflEG&mobile=xxyy&message=xxyy&senderid=xxyy&type=txt
-//http://smshorizon.co.in/api/sendsms.php?user=AMR Fibernet&apikey=lrb00IoktsKI0wqvflEG&message=Test sms API&mobile=9701953773&senderid=MYTEXT&type=txt
-	StringBuilder sbPostData= new StringBuilder(mainUrl);
-	sbPostData.append("user="+user); 
-	sbPostData.append("&apikey="+apikey);
-	sbPostData.append("&message="+message);
-	sbPostData.append("&mobile="+mobile);
-	sbPostData.append("&senderid="+senderid);
-	sbPostData.append("&type="+type);
-
-	//final string
-	mainUrl = sbPostData.toString();
-	try
-	{
-	    //prepare connection
-	    myURL = new URL(mainUrl);
-	    System.out.println(mainUrl);
-	    myURLConnection = myURL.openConnection();
-	    myURLConnection.connect();
-	    reader= new BufferedReader(new InputStreamReader(myURLConnection.getInputStream()));
-	    //reading response 
-	    String response;
-	    while ((response = reader.readLine()) != null) 
-	    //print response 
-	    System.out.println(response);
-	    
-	    //finally close connection
-	    reader.close();
-	} 
-	catch (IOException e) 
-	{ 
-		e.printStackTrace();
-	} 
-}
+public class SMSExternalAPI {
+public static String usersms(String usermobile, int rechargeamount){
+		
+		try 
+		{
+		// Construct data
+		String phonenumbers=usermobile;
+		String data="user=" + URLEncoder.encode("", "UTF-8");
+		data +="&password=" + URLEncoder.encode("", "UTF-8");
+		data +="&message=" + URLEncoder.encode("Javainstances :Dear Customer, your recharge of INR : " +rechargeamount+  "   was successful ", "UTF-8");
+		data +="&sender=" + URLEncoder.encode("JISSMS", "UTF-8");
+		data +="&mobile=" + URLEncoder.encode(phonenumbers, "UTF-8");
+		data +="&type=" + URLEncoder.encode("3", "UTF-8");
+		URL url = new URL("http://login.bulksmsgateway.in/sendmessage.php?"+data);
+		URLConnection conn = url.openConnection();
+		conn.setDoOutput(true);
+		OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+		wr.write(data);
+		wr.flush();
+		// Get the response
+		BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+		String line;
+		String sResult1="";
+		while ((line = rd.readLine()) != null) 
+		{
+		// Process line...
+		sResult1=sResult1+line+" ";
+		}
+		wr.close();
+		rd.close();
+		return sResult1;
+		} 
+		catch (Exception e)
+		{
+		System.out.println("Error SMS "+e);
+		return "ERROR"+e;
+		}
+	}
 }
